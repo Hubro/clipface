e97ec8075c4df74c265110b33e488832d023349b
Tomas Sandven <tomas@sandven.email>
2020-06-06 17:01:06 +0200

    Add relative time column to clip table
    Also messed with table style.

--- app/index.jsx
+++ app/index.jsx
@@ -14,17 +14,19 @@ const IndexPage = ({ videos }) => {
   return (
     <ClipfaceLayout pageName="index" pageTitle="Tomsan clip folder">
       <div className="clip-list">
-        <table className="table is-fullwidth is-hoverable is-bordered">
+        <table className="table is-fullwidth is-bordered">
           <thead>
             <tr>
+              <th>Saved</th>
               <th>Clip name</th>
             </tr>
           </thead>
 
           <tbody>
-            {videos.map((name) => (
-              <tr key={name} onClick={handleLinkClick(name)}>
-                <td>{name}</td>
+            {videos.map((info) => (
+              <tr key={info.filename} onClick={handleLinkClick(info.filename)}>
+                <td>{info.saved_relative}</td>
+                <td>{info.filename}</td>
               </tr>
             ))}
           </tbody>
@@ -35,9 +37,7 @@ const IndexPage = ({ videos }) => {
 };
 
 IndexPage.getInitialProps = async () => {
-  let videos = await fetch("/list-videos").then((resp) => resp.json());
-
-  videos = videos.sort().reverse();
+  const videos = await fetch("/list-videos").then((resp) => resp.json());
 
   return { videos };
 };
--- app/list-videos.py
+++ app/list-videos.py
@@ -1,14 +1,36 @@
 
 import os
+from datetime import datetime
 from pathlib import Path
 
+import timeago
 from flask import jsonify
 
 
 VIDEO_PATH = Path(os.environ.get("VIDEO_PATH", "."))
 
 
+def format_time_ago(dt: datetime) -> str:
+    return timeago.format(dt, datetime.now())
+
+
 def handler():
-    return jsonify(
-        [str(path.name) for path in VIDEO_PATH.glob("*.mkv")]
-    )
+    clips = VIDEO_PATH.glob("*.mkv")
+
+    def get_stats(path: Path):
+        stats = path.stat()
+        mtime = datetime.fromtimestamp(stats.st_mtime)
+
+        return {
+            "filename": path.name,
+            "saved_absolute": mtime,
+            "saved_relative": format_time_ago(mtime),
+            "size": stats.st_size,
+        }
+
+    payload = [get_stats(path) for path in VIDEO_PATH.glob("*.mkv")]
+
+    # Sort the clips by filename, reversed
+    payload = reversed(sorted(payload, key=lambda x: x["filename"]))
+
+    return jsonify(list(payload))

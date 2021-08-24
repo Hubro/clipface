9676b290460c29a1c8a863ce8bd3e5c8933719d8
Tomas Sandven <tomas@sandven.email>
2021-08-24 22:50:12 +0200

    Include bold Roboto font

--- client/src/pages/_document.jsx
+++ client/src/pages/_document.jsx
@@ -8,7 +8,7 @@ class MyDocument extends Document {
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
           <link
-            href="https://fonts.googleapis.com/css2?family=Roboto&display=swap"
+            href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap"
             rel="stylesheet"
           />
 
